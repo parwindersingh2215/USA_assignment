8928eae9cb4461a972c292a1be42f1f2b6c3eb91
ks1664 <kapilsharma52343@gmail.com>
2020-11-12 17:15:49 +0530

    done changes in css

--- AboutUs.php
+++ AboutUs.php
@@ -70,33 +70,6 @@
     </div>
     <div style="text-align: center"><a href="" class="btn about-btn btn-transparent">Learn more</a></div>
 </section>
-
-<footer>
-    <div class="row" style="margin-bottom: -80px">
-        <div style="width: 40%">
-            <a class="footer-title" style="color: white;text-decoration: none;margin-top: 10px" href="index.php">Volunteer
-                Management System</a>
-        </div>
-        <div style="width: 60%">
-            <ul>
-                <li><a href="AboutUs.php">About us</a></li>
-                <li><a href="features.php">Services</a></li>
-                <li><a href="#">Forum</a></li>
-                <li><a href="gallery.php">Gallery</a></li>
-                <li><a href="Testimonials.php">Testimonials</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <div class="row" style="margin-top: 20px">
-        <div class="col-5">
-            <a class="" href="#" style="color: white;text-decoration: none">&#169; Created by Student</a>
-        </div>
-        <div class="col-5" style="text-align: end;">
-            <a class="" href="#" style="color: white;text-decoration: none">All right Reserved</a>
-        </div>
-    </div>
-</footer>
+<?php include "footer.php"; ?>
 </body>
 </html>
